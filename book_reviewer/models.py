92c8f6d04fe11159fe3a534d880cf6f785bd848c
stsh1119 <stshlaptop@gmail.com>
2021-04-01 22:51:50 +0300

    fix: updates to class names, tablenames, constraints

--- book_reviewer/models.py
+++ book_reviewer/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -14,25 +15,25 @@ class User(db.Model):
         return f"User('{self.id}','{self.email}', '{self.last_login_time}')"
 
 
-class Book(db.Model):
-    __tablename__ = 'books'
+class BookReview(db.Model):
+    __tablename__ = 'book_reviews'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
-    review_text = db.Column(db.String(100), nullable=False)
+    review_text = db.Column(db.String(500), nullable=False)
     category = db.Column(db.String(50), default='Uncategorized')
-    creation_date = db.Column(db.DateTime, nullable=False)
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.now())
     user = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
 
     def __repr__(self):
-        return f"Book('{self.title}', '{self.review_text}','{self.category}', '{self.creation_date}')"
+        return f"BookReview('{self.title}', '{self.review_text}','{self.category}', '{self.creation_date}')"
 
 
 class Reaction(db.Model):
     __tablename__ = 'reactions'
     id = db.Column(db.Integer, primary_key=True)
     reacted_user = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
-    reacted_post = db.Column(db.String(50), db.ForeignKey('books.id'), nullable=False)
-    reaction_type = db.Column(db.String(50), nullable=False)
+    reacted_post = db.Column(db.String(50), db.ForeignKey('book_reviews.id'), nullable=False)
+    reaction_type = db.Column(db.String(10), nullable=False)
     creation_date = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
