0d5535b50107fa5b073813120a636c855bf6beea
stsh1119 <stshlaptop@gmail.com>
2021-04-02 22:42:59 +0300

    feat: view all reviews for a book

--- book_reviewer/books/routes.py
+++ book_reviewer/books/routes.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from pydantic import ValidationError
 from book_reviewer.utils import json_body_required
 from .dto import CreateReviewDto
-from .service import add_review
+from .service import add_review, all_reviews_for_book
 
 books = Blueprint('books', __name__)
 
@@ -18,3 +18,9 @@ def add_new_review():
         return jsonify('Book review was added'), 201
     except ValidationError as e:
         return e.json(), 400
+
+
+@books.route("/reviews/<string:book>", methods=['GET'])
+def view_all_reviews_for_a_book(book):
+    result = all_reviews_for_book(book_name=book, page_num=int(request.args.get('page')))
+    return jsonify(result), 200
--- book_reviewer/books/service.py
+++ book_reviewer/books/service.py
@@ -1,13 +1,21 @@
-from ..models import db, User, BookReview
+from ..models import db, User, BookReview, BookReviewModel
 from .dto import CreateReviewDto
 
 
 def add_review(review: CreateReviewDto, email: str) -> None:
     user = User.query.filter_by(email=email).first_or_404()
-    book_review = BookReview(title=review.title,
+    book_review = BookReview(book=review.book,
+                             title=review.title,
                              review_text=review.review_text,
                              category=review.category,
                              user=user.id
                              )
     db.session.add(book_review)
     db.session.commit()
+
+
+def all_reviews_for_book(book_name: str, page_num: int = 1) -> list:
+    books = BookReview.query.filter_by(book=book_name).paginate(page=page_num, per_page=20).items
+    books_list = [BookReviewModel.from_orm(book).dict() for book in books]
+
+    return books_list
