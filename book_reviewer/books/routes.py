f5fbeea810153b70425fcf31d7c3f86b398afbe6
stsh1119 <stshlaptop@gmail.com>
2021-04-04 22:37:47 +0300

    feat: routes to view categories, reviews, like and remove likes

--- book_reviewer/books/routes.py
+++ book_reviewer/books/routes.py
@@ -3,7 +3,9 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from pydantic import ValidationError
 from book_reviewer.utils import json_body_required
 from .dto import CreateReviewDto
-from .service import add_review, all_reviews_for_book
+from .service import (add_review, all_reviews_for_book, all_reviews_for_all_books, all_categories_for_books,
+                      all_reviews_under_certain_category, all_reviews_made_by_user, like_review, unlike_review)
+
 
 books = Blueprint('books', __name__)
 
@@ -16,11 +18,75 @@ def add_new_review():
         review_creation_data = CreateReviewDto.parse_obj(request.json)
         add_review(review_creation_data, email=get_jwt_identity())
         return jsonify('Book review was added'), 201
+
     except ValidationError as e:
         return e.json(), 400
 
+    except Exception as e:
+        return jsonify(str(e)), 400
+
 
 @books.route("/reviews/<string:book>", methods=['GET'])
+@jwt_required(optional=True)
 def view_all_reviews_for_a_book(book):
-    result = all_reviews_for_book(book_name=book, page_num=int(request.args.get('page')))
+    page_num = int(request.args.get('page', default=1))
+    result = all_reviews_for_book(book_name=book, page_number=page_num)
+
     return jsonify(result), 200
+
+
+@books.route("/reviews/all", methods=['GET'])
+@jwt_required(optional=True)
+def view_all_reviews_for_all_books():
+    page_num: int = int(request.args.get('page', default=1))
+    result = all_reviews_for_all_books(page_num)
+
+    return jsonify(result), 200
+
+
+@books.route("/categories", methods=['GET'])
+@jwt_required(optional=True)
+def view_all_categories_for_books():
+    result = all_categories_for_books()
+    return jsonify(result)
+
+
+@books.route("/categories/<string:category_name>", methods=['GET'])
+@jwt_required(optional=True)
+def view_all_reviews_under_given_category(category_name):
+    page_num = int(request.args.get('page', default=1))
+    result = all_reviews_under_certain_category(category_name, page_num)
+
+    return jsonify(result)
+
+
+@books.route("/<string:user>/reviews", methods=['GET'])
+@jwt_required(optional=True)
+def view_all_reviews_under_user(user):
+    page_num = int(request.args.get('page', default=1))
+    result = all_reviews_made_by_user(user, page_num)
+
+    return jsonify(result)
+
+
+@books.route("/my_reviews", methods=['GET'])
+@jwt_required()
+def view_reviews_under_current_user():
+    page_num = int(request.args.get('page', default=1))
+    result = all_reviews_made_by_user(get_jwt_identity(), page_num)
+
+    return jsonify(result)
+
+
+@books.route("/reviews/<int:review_id>/like", methods=['POST'])
+@jwt_required()
+def like_a_review(review_id):
+    result = like_review(review_id, get_jwt_identity())
+    return jsonify(result)
+
+
+@books.route("/reviews/<int:review_id>/unlike", methods=['DELETE'])
+@jwt_required()
+def unlike_a_review(review_id):
+    result = unlike_review(review_id, get_jwt_identity())
+    return jsonify(result)
