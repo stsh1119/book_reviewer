37cf8a223cec949682fb18648c9da5bef6308cd3
stsh1119 <stshlaptop@gmail.com>
2021-04-04 22:39:15 +0300

    fix: change in dto as new table for categories was added and category is int now

--- book_reviewer/books/dto.py
+++ book_reviewer/books/dto.py
@@ -6,4 +6,4 @@ class CreateReviewDto(BaseModel):
     book: str = Field(min_length=5, max_length=50)
     title: str = Field(min_length=5, max_length=50)
     review_text: str = Field(max_length=500)
-    category: Optional[str] = Field(max_length=50)
+    category: Optional[int]
